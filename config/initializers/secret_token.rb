cbdb5ca20b766df1c33421d00c7a8e0d7ab213ee
esanin <esanin@gmail.com>
2013-11-07 22:06:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FridaySoundtrack::Application.config.secret_token = '24c1a7d3e7be36eb72b5f3457a044932ea39751068935eca6db1cd70a1945731a70e87891715711dde714dcc383805d565fbec7497c2cdcdeb99e1ab731f76ae'
